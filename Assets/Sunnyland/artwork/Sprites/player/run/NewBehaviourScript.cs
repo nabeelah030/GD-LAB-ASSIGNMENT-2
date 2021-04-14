9b41bb11e9b344d7a3d910eded63676b25787d62
nabeelah030 <80572116+nabeelah030@users.noreply.github.com>
2021-04-14 09:36:50 +0500

    first commit

---
+++ Assets/Sunnyland/artwork/Sprites/player/run/NewBehaviourScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int speed = 3;
+    public int jS = 10;
+    private Rigidbody2D rigidbody;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mov = Input.GetAxis("Horizontal");
+        animator.SetFloat("Speed", mov * speed);
+        float movVertival = Input.GetAxis("Vertical");
+        rigidbody.velocity= new Vector3(mov * speed, movVertival * speed, 0);
+        float jumpspeed = Input.GetAxis("Jump");
+        Debug.Log(jumpspeed);
+        if(jumpspeed>0)
+        {
+            rigidbody.velocity = new Vector3(rigidbody.velocity.x, jS, 0);
+
+        }
+        else if(jumpspeed > 0)
+        {
+
+        }
+    }
+}
